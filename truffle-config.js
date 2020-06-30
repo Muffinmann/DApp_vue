fd2c901a66fbfcca53d9ca531d09889a749f85d7
conner <conner.zhang.gl@gmail.com>
2020-07-01 00:13:38 +0200

    test of new config

--- truffle-config.js
+++ truffle-config.js
@@ -48,14 +48,14 @@ module.exports = {
      network_id: "*",       // Any network (default: none)
      accounts: 5,
      defaultEtherBalance: 100,
-     gas: 85000000，
+     gas: 85000000,
      gasPrice: 10000000000
     },
 
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 9545,            // Standard Ethereum port (default: none)
-     network_id: "*",       // Any network (default: none)
+     network_id: "*"        // Any network (default: none)
     },
 
     // Another network with more advanced options...
